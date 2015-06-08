53598e997f90d676f046b4e2cf87b03be95c5c64
Kesara Rathnayake <kesara@kesara.lk>
2015-06-08 22:43:17 +1200

    Added message insert view.

--- rosettastones/app.py
+++ rosettastones/app.py
@@ -24,4 +24,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    """
+    Home page
+    """
+    return render_template("index.html")
+
+@app.route("/add")
+def add():
+    """
+    Insert a message
+    """
+    return render_template("add.html")
