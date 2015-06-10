e0ab6b097dccfc4b02c5b5122107fa313fe726a8
Kesara Rathnayake <kesara@kesara.lk>
2015-06-10 18:59:09 +1200

    JavaScript improvements.

--- rosettastones/static/js/gen_keys.js
+++ rosettastones/static/js/gen_keys.js
@@ -1,6 +1,8 @@
 function generate_keys() {
+  $("#generate").html("Generating keys...");
   $.getJSON("/gen_keys", function(data) {
     $("#master_key").text(data.master_key);
     $("#public_key").text(data.public_key);
-  });
+  }).done(function() {
+      $("#generate").html("Generate"); });
 }
